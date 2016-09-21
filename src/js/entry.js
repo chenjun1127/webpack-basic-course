bca688b61beb2af99d9922f0a85961bcff053518
chenjun <chenjun@oeasy.com>
2016-09-21 16:00:55 +0800

    del

--- src/js/entry.js
+++
@@ -1,8 +0,0 @@
-require("../css/style.css")
-document.write('It works.' + '<br/>')
-var Person = require('./module.js')
-document.write(Person.person('jone', 30, 'teacher'));
-
-var img = document.createElement('img');
-img.src = require('../img/pic.png');
-document.body.appendChild(img);
--- src/js/module.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	person: function(name, age, work) {
-		return ('my name is ' + name + ',my age ' + age + ',' + work + ' is my work!');
-	},
-}
